8780894a317bece5ca15ca6dab859ae4635b0169
gustavinho <gustavoatapo@gmail.com>
2024-06-15 11:17:37 -0300

    componentizando cards de anuncio

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MenuItemsComponent } from '../components/menu-items/menu-items.component';
+import { CommonModule } from '@angular/common';
+import { ItemComponentComponent } from '../components/item-component/item-component.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  imports: [MenuItemsComponent, CommonModule, ItemComponentComponent],
 })
 export class HomePageComponent {
+  constructor() {}
 
+  ngOnInit() {}
+
+  tela1 = false;
 }
